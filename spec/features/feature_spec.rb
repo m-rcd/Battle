f9a3e64420bf1509cbc6a85be329a5127febc195
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-18 16:06:57 +0100

    enter players names and display after submit

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 require './app'
 
-RSpec.feature "Testing infrastructure" do
-  scenario "starting homepage" do
+# RSpec.feature "Testing infrastructure" do
+#   scenario "starting homepage" do
+#     visit '/'
+#     expect(page).to have_content("Testing infrastructure working!")
+#   end
+# end
+
+RSpec.feature "Entering player names" do
+  scenario "Players enter their names at the beginning" do
     visit '/'
-    expect(page).to have_content("Testing infrastructure working!")
+    fill_in 'player1', with: 'Marianne'
+    fill_in 'player2', with: 'Cristina'
+    click_button 'Submit'
+    expect(page).to have_content('Marianne vs. Cristina')
   end
 end
